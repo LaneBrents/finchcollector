0c4ff3b2b4394a9d838193d6d502bfa6c05e118c
LaneBrents <lanebrents@gmail.com>
2022-08-30 23:49:08 -0400

    part 3

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+
+def get_absolute_url(self):
+    
+    return reverse('detail', kwargs={'finch_id': self.id})
+
+# Field Choices
+MEALS = ( # This is an example of a constant
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1, # we set up a field choice!
+        choices = MEALS, # dropdown, the value of the dropdown will be 'b' while the text that the human sees will be breakfast, lunch, dinner
+        default = MEALS[0][0] # 'B'
+        )
+
+# Create the cat_id FK (foreign key)
+# on_delete is saying if we delete a cat, delete all of the feedings the finch has, so no feedings exist without a finch
+finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+
+class Meta:
+		ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,17 @@
-from io import BufferedReader
-from unicodedata import name
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+# Import our model
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch
+
+# Create your views here
 from django.http import HttpResponse
 
+from .forms import FeedingForm
 # Add the class of finch
-class Finch:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
 
-finches = [
-    Finch('Lolo', 'tabby', 'smelly', 2),
-    Finch('Moomoo', 'calico', 'angy', 3),
-    Finch('Chester', 'black', 'mysterious', 30)
-]
-# Create your views here.
+
+# Define the home view
 def home(request):
     return HttpResponse('HELLOOOOO')
 
@@ -25,4 +19,38 @@ def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {'finches': finches})
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {'finch': finch})
+
+    feeding_form = FeedingForm()
+
+    return render(request, 'finches/detail.html', {'finch': finch, 'feeding_form': feeding_form})
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+    success_url = '/finches/'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    # Exclude the name from update
+    fields = ['breed', 'description', 'age']
+    success_url = '/finches/'
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches/' 
+
+def add_feeding(request, finch_id):
+    # we need to create a modelForm instance using the data from request.POST 
+    form = FeedingForm(request.POST)
+    # validate the form, so make sure the inputs are of the correct type and shape
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = finch_id
+        new_feeding.save() 
+    return redirect('detail', finch_id=finch_id)
